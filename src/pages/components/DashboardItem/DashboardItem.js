0a1e879586ca15924327686a601cfaf33d5abcae
cVidalSP <kainan.vidal@gmail.com>
2019-11-20 18:26:48 -0300

    Alteracao de layout

---
+++ src/pages/components/DashboardItem/DashboardItem.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Service from '../../../services/BaseAxios';
+import './DashboardItem.css';
+
+export default function Dashboard(){
+
+    return(
+        <div className="mainContent">
+            <p>
+                Petições ja cadastradas:
+            </p>
+
+        </div>
+    )
+}
+
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Login from './pages/Login/Login.js';
 import NewPetition from './pages/Newpetition/NewPetition.js';
+import Dashboard from './pages/components/DashboardItem/DashboardItem.js';
 
 export default function Routes(){
     return(
@@ -10,6 +11,7 @@ export default function Routes(){
             <Switch>
                 <Route path="/" exact component={Login} />
                 <Route path="/newPetition" exact component={NewPetition} />
+                <Route path="/dashboard" exact component={Dashboard} />
             </Switch>
         </BrowserRouter>
     );
