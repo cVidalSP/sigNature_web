fbc5b86ac49256398a3044eae887cfe0738d2641
cVidalSP <kainan.vidal@gmail.com>
2019-11-20 18:08:55 -0300

    Alteracao de layout

--- src/pages/Newpetition/NewPetition.js
+++ src/pages/Newpetition/NewPetition.js
@@ -1,45 +1,61 @@
 import React, { useState } from 'react';
-import { TextInput, Button } from 'grommet';
+// import { TextInput, Button } from 'grommet';
 import Service from '../../services/BaseAxios';
+
+import './NewPetition.css';
 
 export default function NewPetition(){
     const [name, setName] = useState('');
     const [desc, setDesc] = useState('');
-    const [qtAss, setQtAss] = useState(0);
     const [image, setimage] = useState('');
 
     function setPetition() {
-        Service.post('http://localhost:3333/petitions', { name, desc, qtAss, image}).then(function (value) {
+        Service.post('http://localhost:3333/petitions', { name, desc, image}).then(function (value) {
             alert(JSON.stringify(value))
         });
     }
 
+    function handleSubmit(){
+
+    }
+
     return (
-        <>
-        <TextInput
-            placeholder="Nome da petição"
-            value={name}
-            onChange={event => setName(event.target.value)}
-            />
-            <TextInput
-            placeholder="Descrição da petição"
-            value={desc}
-            onChange={event => setDesc(event.target.value)}
-            />
-            <TextInput
-            placeholder="Quantidade de assinaturas, default: 0"
-            value={qtAss}
-            onChange={event => setQtAss(event.target.value)}
-            />
-            <TextInput
-            placeholder="Imagem de exemplo"
-            value={image}
-            onChange={event => setimage(event.target.value)}
-            />
-            <Button
-                label="Enviar"
-                onClick={setPetition}
-            />
-        </>
+        <div className='mainContent'> 
+            <p>
+            Cadastre uma nova <strong>petição</strong>.
+            </p>
+
+            <form onSubmit={handleSubmit}>
+
+                <label htmlFor="nomePeticao">Nome da petição *</label>
+                <input 
+                    id="nomePeticao" 
+                    type="text" 
+                    placeholder="Nome da petição"
+                    value={name}
+                    onChange={event => setName(event.target.value)}
+                />
+
+                <label htmlFor="descricao">Descrição *</label>
+                <input 
+                    id="descricao" 
+                    type="text" 
+                    placeholder="Descrição da petição"
+                    value={desc}
+                    onChange={event => setDesc(event.target.value)}
+                />
+
+                <label htmlFor="imagem">URL da imagem *</label>
+                <input 
+                    id="imagem" 
+                    type="text" 
+                    placeholder="URL"
+                    value={image}
+                    onChange={event => setimage(event.target.value)}
+                />
+
+                <button className="btn" type="submit">Cadastrar</button>
+            </form>
+        </div>
     );
 }
--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Login from './pages/Login/Login.js';
+import NewPetition from './pages/Newpetition/NewPetition.js';
 
 export default function Routes(){
     return(
         <BrowserRouter> 
             <Switch>
                 <Route path="/" exact component={Login} />
+                <Route path="/newPetition" exact component={NewPetition} />
             </Switch>
         </BrowserRouter>
     );
